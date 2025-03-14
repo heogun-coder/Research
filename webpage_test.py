4c4fdf0b475f74fcc70e6160fe30d09a41326e5c
unknown <heochangick@gmail.com>
2025-03-14 09:21:13 +0900

    v2

--- webpage_test.py
+++ webpage_test.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify, render_template
 
-app = Flask()
+app = Flask(__name__)
 
 
 @app.route("/")
 def home():
     return render_template("")
+
+
+if __name__ == "main":
+    app.run()
